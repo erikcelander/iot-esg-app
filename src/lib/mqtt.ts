52ee95051e9cd8195516eea44f954c91c6039624
Henrik Tunedal <ht222mv@student.lnu.se>
2024-05-22 23:58:07 +0200

    wip

--- src/lib/mqtt.ts
+++ src/lib/mqtt.ts
@@ -17,8 +17,6 @@ export const useMqtt = (setID: string, nodeID: string, onMessage: Function) => {
 
 function createMqttConnection(url: string, username: string, password: string): Connection {
   const subscribers: Map<string, Function[]> = new Map();
-  const clientSubscriptions = new Set<string>();
-
   let currentClient: MqttClient | null = null;
 
   function createClient() {
@@ -33,84 +31,70 @@ function createMqttConnection(url: string, username: string, password: string): 
 
       console.log("Client connected.");
       Array.from(subscribers.keys())
-        .filter(topic => !clientSubscriptions.has(topic))
-        .forEach(topic => subscribeClient(topic));
+        .filter(topic => !subscribers.has(topic))
+        .forEach(topic => addSubscriptionTopic(client, topic));
     });
 
     client.on("message", (topic, message) => {
-      const callbacks = subscribers.get(topic) ?? [];
-      callbacks.forEach(callback => callback(topic, message))
+      const callbacks = subscribers.get(topic);
+      callbacks?.forEach(callback => callback(topic, message))
     });
-
-    client.on("message", (topic, message) => { console.log("Noch einmal!") });
-    client.on("message", (topic, message) => { console.log("Zai shuo yibian!") });
-    client.on("message", (topic, message) => { console.log("Mou ikkai!") });
 
     return client;
   }
 
   function subscribeClient(topic: string) {
-    let client = currentClient;
-    if (client === null) {
-      client = currentClient = createClient();
-    }
+    const client = currentClient === null
+      ? currentClient = createClient()
+      : currentClient;
 
     if (client.connected) {
-      console.log("Subscribing client to topic:", topic);
-
-      client.subscribe(topic, (err) => {
-        if (err) {
-          console.error("Failed to subscribe to topic", err);
-        } else {
-          clientSubscriptions.add(topic);
-          console.log("Subscribed to topic:", topic);
-        }
-      });
+      addSubscriptionTopic(client, topic);
     }
   }
 
   function unsubscribeClient(topic: string) {
     console.log("Unsubscribing client from topic:", topic);
     currentClient?.unsubscribe(topic);
-    clientSubscriptions.delete(topic);
   }
 
-  function disconnectClient() {
-    console.log("Disconnecting from MQTT broker");
-    currentClient?.end();
-    clientSubscriptions.clear();
-    currentClient = null;
+  function addSubscriptionTopic(client: mqtt.MqttClient, topic: string) {
+    console.log("Subscribing client to topic:", topic);
+    client.subscribe(topic, (err) => {
+      if (err) {
+        console.error("Failed to subscribe to topic", err);
+      } else {
+        console.log("Subscribed to topic:", topic);
+      }
+    });
   }
 
   return {
     subscribe(topic: string, onMessage: Function): Subscription {
       console.log("Adding subscription callback for topic:", topic);
 
-      let callbacks = subscribers.get(topic);
-      if (callbacks === null || callbacks === undefined) {
-        callbacks = [];
-        subscribers.set(topic, callbacks);
+      if (!subscribers.has(topic)) {
+        subscribers.set(topic, []);
       }
+      const callbacks = subscribers.get(topic)!;
       callbacks.push(onMessage);
 
-      if (!clientSubscriptions.has(topic)) {
+      if (callbacks.length === 1) {
         subscribeClient(topic);
       }
 
       return {
         unsubscribe() {
           console.log("Removing subscription callback for topic:", topic);
+
           const callbacks = subscribers.get(topic) ?? [];
           const index = callbacks.indexOf(onMessage);
           if (index > -1) {
             callbacks.splice(index, 1);
           }
+
           if (callbacks.length === 0) {
             unsubscribeClient(topic);
-          }
-          if (clientSubscriptions.size === 0) {
-            console.log("No more subscribers!");
-            //disconnectClient();
           }
         }
       }
