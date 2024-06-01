6fe5629811dda96acabb091b28dbe49a06279b2a
Henrik Tunedal <ht222mv@student.lnu.se>
2024-06-02 00:21:25 +0200

    wip

--- api/esg.py
+++ api/esg.py
@@ -3,11 +3,11 @@ import sys
 def app(environ, respond):
     respond("200 OK", [("Content-Type", "text/plain")])
     lines = []
-    lines.append(b"Hello from Python!")
+    lines.append("Hello from Python!")
     lines.append(repr(sys.path))
     try:
         import openpyxl
         lines.append(repr(openpyxl))
     except Exception as ex:
         lines.append(repr(ex))
-    return [b"\n".join(lines)]
+    return ["\n".join(lines).encode("utf-8")]
