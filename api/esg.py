5d259dcc0fc7e7a8e4b00d897dea509ea0fc9346
Henrik Tunedal <ht222mv@student.lnu.se>
2024-06-02 01:11:40 +0200

    wip

--- api/esg.py
+++ api/esg.py
@@ -3,11 +3,16 @@ import sys
 def app(environ, respond):
     respond("200 OK", [("Content-Type", "text/plain")])
     lines = []
+
     lines.append("Hello from Python!")
     lines.append(repr(sys.path))
+
     try:
         import openpyxl
         lines.append(repr(openpyxl))
     except Exception as ex:
         lines.append(repr(ex))
+
+    print(environ)
+
     return ["\n".join(lines).encode("utf-8")]
