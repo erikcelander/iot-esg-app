10bd515554aea0e757b06066f25663f86c29ffd3
Henrik Tunedal <ht222mv@student.lnu.se>
2024-06-02 18:12:54 +0200

    wip

--- src/lib/pythonCallHelper.ts
+++ src/lib/pythonCallHelper.ts
@@ -6,27 +6,36 @@ export const callPython = process.env.VERCEL_URL
   ? callVercelServerlessFunction
   : callSubprocess
 
+async function callVercelServerlessFunction(name: String): Promise<Buffer> {
+  // Reuse the shared cookie secret as authorization token.
+  let token = process.env.SECRET_COOKIE_PASSWORD!
+
+  let url = `https://${process.env.VERCEL_URL}/api/${name}`
+  let response = await fetch(url, {
+    method: "POST",
+    headers: {
+      Authorization: `Bearer ${token}`,
+      "Content-Type": "application/octet-stream",
+    },
+  })
+
+  return Buffer.from(await response.arrayBuffer())
+}
+
 async function callSubprocess(name: String): Promise<Buffer> {
     // Must be imported dynamically because the module module might be
     // imported by the middleware (among other things) which Next.js wants
     // to run on the edge runtime, which doesn't support this API.
-    const execFile = promisify((await import("child_process")).execFile)
+    let execFile = promisify((await import("child_process")).execFile)
 
-    const args = [`./api/${name}.py`]
-    const options = {
+    let args = [`./api/${name}.py`]
+    let options = {
       maxBuffer: 16384 * 1024,
       encoding: "buffer",
     }
-    const { stdout, stderr } = await execFile("python3", args, options)
+    let { stdout, stderr } = await execFile("python3", args, options)
     console.log("stdout:", stdout)
     console.log("stderr:", stderr)
 
     return stdout as unknown as Buffer
-}
-
-async function callVercelServerlessFunction(name: String): Promise<Buffer> {
-    // Reuse the shared cookie secret for authentication.
-    let secret = process.env.SECRET_COOKIE_PASSWORD!
-
-    return Buffer.from("Hello from server action!", "utf-8")
 }
