efb28627ed841e9c3219bf8de455d07e74f5b7cb
Henrik Tunedal <ht222mv@student.lnu.se>
2024-06-02 16:44:48 +0200

    Revert "wip broken form for post"
    This reverts commit 8c115afc044bab34d47484bac372bd97df0e7b1d.

--- src/app/api/esg-report/route.ts
+++ src/app/api/esg-report/route.ts
@@ -1,16 +1,10 @@
 import { NextRequest, NextResponse } from "next/server";
 
-export const runtime = "nodejs";
-
 export async function POST(req: NextRequest) {
   //const data = await req.json();
   //console.log("data  ", data);
 
   let data = "Hello from my JS route!";
-  console.log(data);
 
-  let headers = {
-    "Content-Type": "text/plain",
-  }
-  return new NextResponse(`${data}`, {status: 220, headers});
+  return new NextResponse(`${data}`);
 }
--- src/components/home/property-owner/property-owner.tsx
+++ src/components/home/property-owner/property-owner.tsx
@@ -14,23 +14,8 @@ export function PropertyOwner({ accessToken }: { accessToken: string }) {
     setIsLoading(false);
   }
 
-  async function onSubmit(event: FormEvent<HTMLFormElement>) {
-    event.preventDefault()
-
-    const formData = new FormData(event.currentTarget)
-    const response = await fetch('/api/esg-report', {
-      method: 'POST',
-      body: formData,
-    })
-
-    // Handle response if necessary
-    const data = await response.json()
-    // ...
-  }
-
   return <div className="flex w-full h-full flex-col  justify-center items-center">
     <h1 className="text-2xl pt-4 pb-4">Download ESG report</h1>
-    <form onSubmit={onSubmit}>
     <Button className="rounded-xl text-base w-50" onClick={handlereport} disabled={isLoading}
       style={{ width: "20em" }}>
       {isLoading ? (
@@ -41,18 +26,5 @@ export function PropertyOwner({ accessToken }: { accessToken: string }) {
         "Download report"
       )}
     </Button>
-    </form>
   </div>;
 };
-
-
-import { FormEvent } from 'react'
-
-export default function Page() {
-  return (
-    <form onSubmit={onSubmit}>
-      <input type="text" name="name" />
-      <button type="submit">Submit</button>
-    </form>
-  )
-}
